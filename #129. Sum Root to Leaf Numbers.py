0913c7a6e3a8715062a7e2694d912af32f3a9173
Stephen George <stephengeorge2640@gmail.com>
2022-05-06 21:33:50 +0530

    Added code for 129

--- #129. Sum Root to Leaf Numbers.py
+++ #129. Sum Root to Leaf Numbers.py
@@ -6,18 +6,13 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        
-        def helper(node,n):
+
+        def helper(node, n):
             if not node:
                 return 0
-            
-            n=n*10+node.val
+
+            n = n*10+node.val
             if not node.left and not node.right:
                 return n
-            return helper(node.left,n) + helper(node.right,n)
-        return helper(root,0)
-            
-            
-            
-            
-        
+            return helper(node.left, n) + helper(node.right, n)
+        return helper(root, 0)
