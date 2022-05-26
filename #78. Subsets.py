766efc34816383977a500d7aef8a36cae9bfdd47
Stephen George <stephengeorge2640@gmail.com>
2022-05-26 11:18:17 +0530

    78 with code

--- #78. Subsets.py
+++ #78. Subsets.py
@@ -1,18 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res=[]
-        subset=[]
+        res = []
+        subset = []
+
         def helper(i):
-            if i>=len(nums):
+            if i >= len(nums):
                 res.append(subset.copy())
                 return
-            
+
             subset.append(nums[i])
             helper(i+1)
-            
+
             subset.pop()
             helper(i+1)
-            
+
         helper(0)
 
         return res
