ebf3a027de78d2d4d6a53261ac5b3869c9978305
Stephen George <stephengeorge2640@gmail.com>
2022-08-24 20:23:29 +0530

    863

--- #863. All Nodes Distance K in Binary Tree.py
+++ #863. All Nodes Distance K in Binary Tree.py
@@ -6,60 +6,57 @@
 #         self.right = None
 
 class Solution:
-    
-    def graph(self,root):
-        mp={}
-        q=collections.deque([root])
+
+    def graph(self, root):
+        mp = {}
+        q = collections.deque([root])
         while q:
-            curr=q.popleft()
-    
+            curr = q.popleft()
+
             if curr not in mp:
-                mp[curr]=[]
-            
+                mp[curr] = []
+
             if curr.left:
                 #add child to parent adjList
                 mp[curr].append(curr.left)
                 #add parent to child adjList
                 if curr.left not in mp:
-                    mp[curr.left]=[]
+                    mp[curr.left] = []
                 mp[curr.left].append(curr)
                 #add child to queue
                 q.append(curr.left)
-            
+
             if curr.right:
                 #add child to parent adjList
                 mp[curr].append(curr.right)
                 #add parent to child adjList
                 if curr.right not in mp:
-                    mp[curr.right]=[]
+                    mp[curr.right] = []
                 mp[curr.right].append(curr)
                 #add child to queue
-                q.append(curr.right)       
+                q.append(curr.right)
         return mp
-                
-        
-    
-    
+
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        
+
         #edge case
         if not root or not target:
             return []
-        
+
         if k == 0:
             return [target.val]
-        
-        result=[]
-        adjL=self.graph(root)
-        
+
+        result = []
+        adjL = self.graph(root)
+
         #BFS of the adjacency list
-        q=collections.deque([target])
-        d=0
-        visited=set()
-        while q and d<=k:
-            qLen=len(q)
+        q = collections.deque([target])
+        d = 0
+        visited = set()
+        while q and d <= k:
+            qLen = len(q)
             for _ in range(qLen):
-                node=q.popleft()
+                node = q.popleft()
                 if node not in visited:
                     for child in adjL[node]:
                         q.append(child)
@@ -67,8 +64,7 @@ class Solution:
                     if d == k:
                         result.append(node.val)
                 visited.add(node)
-                    
-            d+=1
+
+            d += 1
 
         return result
-        
