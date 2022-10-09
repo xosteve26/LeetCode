edc95ed6d3b4c45c0002cfa924cb469f57befcfd
Stephen George <stephengeorge2640@gmail.com>
2022-10-09 22:32:13 +0530

    743

--- #743. Network Delay Time.py
+++ #743. Network Delay Time.py
@@ -1,27 +1,27 @@
+import heapq
 class Solution:
     def networkDelayTime(self, times: List[List[int]], length: int, k: int) -> int:
-        
-        adjList={i:[] for i in range(length+1)}
-        
-        for s,d,w in times:
-            adjList[s].append((d,w))
-            
-        result=0
-        shortest={}
-        minHeap=[(0,k)]
-        
+
+        adjList = {i: [] for i in range(length+1)}
+
+        for s, d, w in times:
+            adjList[s].append((d, w))
+
+        result = 0
+        shortest = {}
+        minHeap = [(0, k)]
+
         while minHeap:
-            w,n=heapq.heappop(minHeap)
-            
+            w, n = heapq.heappop(minHeap)
+
             if n in shortest:
-                continue   
-                
-            shortest[n]=w
-            result=max(result,w)
-            
-            for node,weight in adjList[n]:
+                continue
+
+            shortest[n] = w
+            result = max(result, w)
+
+            for node, weight in adjList[n]:
                 if node not in shortest:
-                    heapq.heappush(minHeap,(w+weight, node))
-        
-        return result if len(shortest)==length else -1
-        
+                    heapq.heappush(minHeap, (w+weight, node))
+
+        return result if len(shortest) == length else -1
