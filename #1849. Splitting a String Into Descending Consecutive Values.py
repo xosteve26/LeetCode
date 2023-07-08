64bf2e875b9d654f90f5be00c7fa55f00c588e85
Stephen George <stephengeorge2640@gmail.com>
2023-07-08 22:08:07 +0530

    1849

---
+++ #1849. Splitting a String Into Descending Consecutive Values.py
@@ -0,0 +1,15 @@
+class Solution:
+    def splitString(self, s: str) -> bool:
+
+        def backtrack(i, prev):
+            if i==len(s): return True
+
+            for j in range(i, len(s)):
+                val = int(s[i:j+1])
+                if val+1==prev and backtrack(j+1, val): return True
+            return False
+
+        for i in range(len(s)-1):
+            val = int(s[:i+1])
+            if backtrack(i+1, val): return True
+        return False
